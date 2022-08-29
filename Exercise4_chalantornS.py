02320e66ee96065d99f8cec7b84e35ade39d2108
artF412 <Chalantorn.sup@gmail.com>
2022-08-29 23:55:09 +0700

    Create Exercise4_chalntornS
    collect information test score

---
+++ Exercise4_chalantornS.py
@@ -0,0 +1,9 @@
+fe = 78.6
+gb = 69.4
+ics = 82.7
+cp = 85.2
+print("--- Your Score --- ")
+print("Foundation English : ",fe)
+print("General Business : ",gb)
+print("Introduction to Computer Systems : ",ics)
+print("Computer Programming : ",cp)
