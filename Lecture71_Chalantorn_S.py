77acbfe4dcae28a67d56cdf238b5249f4000e26b
artF412 <Chalantorn.sup@gmail.com>
2022-09-18 14:30:17 +0700

    Create Lecture71_Chalantorn_S.py

---
+++ Lecture71_Chalantorn_S.py
@@ -0,0 +1,20 @@
+menuList = []
+priceList = []
+
+def showBill():
+    txt = "My Restaurants"
+    cen = txt.center(24, "-")
+    print(cen)
+    for num in range(len(menuList)):
+        print(menuList[num],priceList[num])
+
+while True:
+    menuName = input("Please Enter Menu : ")
+    if menuName.lower() == "exit":
+        break
+    else:
+        menuPrice = int(input("Price : "))
+        menuList.append(menuName)
+        priceList.append(menuPrice)
+showBill()
+print("ราคาอาหารทั้งหมด",sum(priceList),"บาท")
