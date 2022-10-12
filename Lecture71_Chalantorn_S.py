913ab39fdd0f58a885cdd7779141ba05db188b84
artF412 <Chalantorn.sup@gmail.com>
2022-10-12 18:17:41 +0700

    Update Lecture71_Chalantorn_S.py

--- Lecture71_Chalantorn_S.py
+++ Lecture71_Chalantorn_S.py
@@ -1,20 +1,21 @@
 menuList = []
-priceList = []
 
 def showBill():
     txt = "My Restaurants"
     cen = txt.center(24, "-")
+    total = 0
     print(cen)
     for num in range(len(menuList)):
-        print(menuList[num],priceList[num])
-    print("ราคาอาหารทั้งหมด",sum(priceList),"บาท")    
+        print(menuList[num][0],menuList[num][1])
+        total += int(menuList[num][1])
+    print('ราคารวม',total,'บาท')
 
 while True:
     menuName = input("Please Enter Menu : ")
     if menuName.lower() == "exit":
         break
     else:
-        menuPrice = int(input("Price : "))
-        menuList.append(menuName)
-        priceList.append(menuPrice)
+        menuPrice = input("Price : ")
+        menuList.append([menuName,menuPrice])
+#print(menuList)
 showBill()
